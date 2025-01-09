b57db6ced9681f7521d7215ef5614e08085bfd14
VSobolenko <v.sobolenko.vs@gmail.com>
2025-01-10 02:12:13 +0300

    initial commit

---
+++ Assets/Scripts/AStarPathFinding.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PathFinding.AStar
+{
+    public class AStarPathFinding
+    {
+        public Grid Grid { get; }
+
+        public AStarPathFinding(Grid grid)
+        {
+            Grid = grid;
+        }
+
+        public bool TryFindPath(Node start, Node end, out Node[] path)
+        {
+            path = Array.Empty<Node>();
+            var capacity = Mathf.Abs(start.X - end.X);
+            var openSet = new List<Node>(capacity);
+            var closedSet = new HashSet<Node>(capacity);
+
+            openSet.Add(start);
+
+            while (openSet.Count > 0)
+            {
+                var currentNode = openSet[0];
+                for (var i = 1; i < openSet.Count; i++)
+                {
+                    if (openSet[i].FCost < currentNode.FCost ||
+                        (openSet[i].FCost == currentNode.FCost && openSet[i].HCost < currentNode.HCost))
+                    {
+                        currentNode = openSet[i];
+                    }
+                }
+
+                openSet.Remove(currentNode);
+                closedSet.Add(currentNode);
+
+                if (currentNode == end)
+                {
+                    path = RetracePath(start, end);
+                    return true;
+                }
+
+                foreach (var neighbor in Grid.GetNeighbors(currentNode))
+                {
+                    if (!neighbor.Walkable || closedSet.Contains(neighbor))
+                        continue;
+
+                    var newMovementCostToNeighbor = currentNode.GCost + GetDistance(currentNode, neighbor);
+                    if (newMovementCostToNeighbor < neighbor.GCost || openSet.Contains(neighbor) == false)
+                    {
+                        neighbor.GCost = newMovementCostToNeighbor;
+                        neighbor.HCost = GetDistance(neighbor, end);
+                        neighbor.Parent = currentNode;
+
+                        if (openSet.Contains(neighbor) == false)
+                            openSet.Add(neighbor);
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private static int GetDistance(Node nodeA, Node nodeB)
+        {
+            var dstX = Mathf.Abs(nodeA.X - nodeB.X);
+            var dstY = Mathf.Abs(nodeA.Y - nodeB.Y);
+
+            if (dstX > dstY)
+                return 14 * dstY + 10 * (dstX - dstY);
+
+            return 14 * dstX + 10 * (dstY - dstX);
+        }
+
+        private static Node[] RetracePath(Node startNode, Node endNode)
+        {
+            var path = new List<Node>();
+            var currentNode = endNode;
+
+            while (currentNode != startNode)
+            {
+                path.Add(currentNode);
+                currentNode = currentNode.Parent;
+            }
+
+            path.Reverse();
+
+            return path.ToArray();
+        }
+    }
+}
---
+++ Assets/Scripts/PathVisualizer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace PathFinding.AStar
+{
+    public class PathVisualizer : MonoBehaviour
+    {
+        [SerializeField] private Vector2Int gridSize = new(10, 10);
+        [SerializeField] private float nodeSize = 1f;
+        [SerializeField] private Transform start, end;
+        [SerializeField, Range(0, 100)] private float wallPercentage = 30f;
+
+        private AStarPathFinding _pathFinding;
+
+        private void Start()
+        {
+            GenerateGrid();
+        }
+
+        [ContextMenu("Re-Generate Grid")]
+        private void GenerateGrid()
+        {
+            var grid = new Grid(gridSize.x, gridSize.y);
+            foreach (Node node in grid)
+                node.Walkable = Random.value > wallPercentage / 100;
+            _pathFinding = new AStarPathFinding(grid);
+            _debugGizmosPath = Array.Empty<Node>();
+        }
+
+        private void Update()
+        {
+            _debugGizmosPath = null;
+            if (start == null || end == null)
+                return;
+
+            var startNode = NodeFromWorldPoint(start.localPosition, _pathFinding.Grid);
+            var endNode = NodeFromWorldPoint(end.localPosition, _pathFinding.Grid);
+
+            if (_pathFinding.TryFindPath(startNode, endNode, out var path) == false)
+                return;
+            _debugGizmosPath = path;
+        }
+
+        private Node NodeFromWorldPoint(Vector3 worldPosition, Grid grid)
+        {
+            var percentX = (worldPosition.x + (float) gridSize.x / 2) / gridSize.x;
+            var percentY = (worldPosition.z + (float) gridSize.y / 2) / gridSize.y;
+            percentX = Mathf.Clamp01(percentX);
+            percentY = Mathf.Clamp01(percentY);
+
+            var gridSizeX = Mathf.RoundToInt(gridSize.x / nodeSize);
+            var gridSizeY = Mathf.RoundToInt(gridSize.y / nodeSize);
+            var x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
+            var y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
+            x = Mathf.Clamp(x, 0, grid.XMax - 1);
+            y = Mathf.Clamp(y, 0, grid.YMax - 1);
+            Debug.Log($"{x}|{y}");
+            return grid[x, y];
+        }
+
+        private Vector3 WorldPointFromNode(Node node)
+        {
+            var worldBottomLeft =
+                transform.position - Vector3.right * gridSize.x / 2 - Vector3.forward * gridSize.y / 2;
+            var nodeRadius = nodeSize / 2;
+            var worldPoint = worldBottomLeft + Vector3.right * (node.X * nodeSize + nodeRadius) +
+                             Vector3.forward * (node.Y * nodeSize + nodeRadius);
+
+            return worldPoint;
+        }
+
+        [SerializeField, Range(0, 1)] private float debugAlpha = 0.7f;
+        private Node[] _debugGizmosPath;
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireCube(transform.position, new Vector3(gridSize.x, 1f, gridSize.y));
+            if (_pathFinding == null)
+                return;
+
+            foreach (Node node in _pathFinding.Grid)
+            {
+                var color = node.Walkable ? Color.white : Color.red;
+
+                if (_debugGizmosPath != null && _debugGizmosPath.ToList().Contains(node))
+                    color = Color.black;
+
+                color.a = debugAlpha;
+                Gizmos.color = color;
+                Gizmos.DrawCube(WorldPointFromNode(node), Vector3.one * (nodeSize - 0.1f));
+            }
+        }
+    }
+}
